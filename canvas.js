cd10ac460f8bc1e11ab43cb3d910079ef5609568
Ryan Volkin <ryanvolkin@gmail.com>
2023-10-28 10:38:13 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -40,7 +40,7 @@ class Player {
         this.speed = 3;
         this.tempWall = {
             orientation : 0, 
-            wall : new Wall(0,0,0,0),
+            wall : new Wall(0,0,0,0,true),
         };
     }
     draw () {
@@ -86,14 +86,10 @@ class Player {
 
             let touchWall=false;
             if (keys.get("mouse")==true){
-                if (this.tempWall.wall.touchingPlayer(this)==false)
+                this.tempWall.wall.makeID();
+                if (this.tempWall.wall.touchingPlayer(this)==false&&addWall(walls,this.tempWall.wall))
                 {
-                    walls.forEach( a => {if (JSON.stringify(a.position) == JSON.stringify(this.tempWall.wall.position))touchWall = true;});
-                    if (touchWall==false)            
-                    {
-                        walls.push(this.tempWall.wall);
-                        this.tempWall.wall = new Wall();
-                    }
+                    this.tempWall.wall = new Wall();
                 }
             }
 
@@ -135,19 +131,30 @@ class Player {
 }
 
 class Wall{
-    constructor(px,py, px2, py2) {
+    constructor(px,py, px2, py2, draw) {
+        let x = px;
+        let y = py;
+        this.canDraw= draw;
         this.position = {
-            x1:px,
-            y1:py,
+            x1:x,
+            y1:y,
             x2:px2,
             y2:py2,
         }
         this.thickness = 10;
         this.health=50;
+        
+    }
+    makeID()
+    {       
+         this.id = parseInt(""+0+this.position.x1+this.position.y1+this.position.x2+this.position.y2);
     }
     draw () {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x1 - this.thickness/2 - center.x + canvas.width/2, this.position.y1 - this.thickness/2 - center.y + canvas.height/2, this.position.x2 - this.position.x1 + this.thickness, this.position.y2 - this.position.y1 + this.thickness);
+        if (!(this.canDraw==false))
+        {
+            c.fillStyle = 'red';
+            c.fillRect(this.position.x1 - this.thickness/2 - center.x + canvas.width/2, this.position.y1 - this.thickness/2 - center.y + canvas.height/2, this.position.x2 - this.position.x1 + this.thickness, this.position.y2 - this.position.y1 + this.thickness);
+        }
     }
     collisionX(player){
         if (player.position.y+player.dimensions.height>this.position.y1 - this.thickness/2  && player.position.y<this.position.y2 + this.thickness/2)
@@ -205,32 +212,72 @@ class Wall{
 }
 
 class Tree{
-    constructor(px,py,px2,py2)
+    constructor(px,py,w,h)
     {
         this.position = {
-            x1:px,
-            y1:py,
-            x2:px2,
-            y2:py2,
+            x:px,
+            y:py,
         }
-        this.health = 1000; 
+        this.dimensions = {
+            width:w,
+            height:h
+        }
+        this.health = 1000;
     }
     draw (){
         c.fillStyle = 'green';
-        c.fillRect(this.position.x1 - center.x + canvas.width/2, this.position.y1 - center.y + canvas.height/2, this.position.x2 - this.position.x1, this.position.y2 - this.position.y1);
+        c.fillRect(this.position.x - center.x + canvas.width/2, this.position.y - center.y + canvas.height/2, this.dimensions.width, this.dimensions.height);
     }
-    addWalls (walls)
+    makeWalls(walls)
     {
-
+        
+        for( let x= this.position.x; x < this.position.x+this.dimensions.width; x+=100)
+        {
+            for ( let y = this.position.y; y < this.position.y+this.dimensions.height; y+=100)
+            {
+                if (y >  this.position.y && y < this.position.y+this.dimensions.height)
+                {
+                    let temp =  new Wall(x,y,x+100,y,false);
+                    temp.makeID();
+                    addWall(walls,temp);
+                }
+                if (x > this.position.x && this.position.x+this.dimensions.width)
+                {
+                    let temp =  new Wall(x,y,x,y+100,false);
+                    temp.makeID();
+                    addWall(walls,temp);
+                }
+            }
+        }
     }
 }
 
-    
+function addWall(walls, wall){
+    let start = 0, end = walls.length-1;
+    while (start <= end){
+        let mid =Math.floor((start+end)/2);
+        if (walls[mid].id == wall.id) return false;
+        else if (walls[mid].id < wall.id)
+            start = mid+1;
+        else
+            end = mid-1;
 
+    }
+    walls.push(wall)
+    walls.sort(compareID);
+    return true;
+}
 
-const p = new Player(canvas.width/2,canvas.height/2);
+function compareID (a,b)
+{
+    return a.id - b.id;
+}
+
+const p = new Player(10000,10000);
 let walls = [];
-let lifeTree = new Tree (500,500,700,700);
+let lifeTree = new Tree (10100,10100,200,200);
+lifeTree.makeWalls(walls);
+console.log(walls);
 function up (event)
 {
     keys.set(event.key.toLowerCase(),false);
@@ -255,11 +302,20 @@ function follow (event)
 
 function animate()
 {
+    
+    const start = performance.now();
     window.requestAnimationFrame(animate)
     c.clearRect(0,0,canvas.width,canvas.height)
+    
     p.draw();
     walls.forEach( a => a.draw());
-    p.actions(walls);     
+    lifeTree.draw();
+
+    p.actions(walls);
+
+    const end = performance.now();
+    //console.log(`Execution time: ${end - start} ms`);
+
 }
 function gameSetUp()
 {
